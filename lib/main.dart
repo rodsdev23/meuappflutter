c618b3bde126d48eec09c8824253d45c20b95c9f
rodsdev23 <rodsdev23@gmail.com>
2023-08-21 01:05:48 -0300

    desafio: subindo projeto flutter no git

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-        hoverColor: Colors.lightGreen,
+        hoverColor: Colors.lightBlue,
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
